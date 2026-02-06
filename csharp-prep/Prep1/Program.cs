a9bb535727b6d7c2d3fa0c559c8bb49dbf7087f0
antonhilmisson-lgtm <antonhilmisson@byui.edu>
2026-02-05 17:50:29 -0700

    git commit "prep 2"

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("what is your name?");
+        String first = Console.ReadLine();
+        first = char.ToUpper(first[0]) + first.Substring(1);
+
+        Console.WriteLine("what is your last name");
+        String Last = Console.ReadLine();
+        Last = char.ToUpper(Last[0]) + Last.Substring(1);
+
+        Console.WriteLine($"so your name is {first} {Last}? ");
+        Console.WriteLine($"It is nice to meet you {first}.");
     }
 }
